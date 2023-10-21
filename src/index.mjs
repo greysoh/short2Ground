67dde8de60405ccbdd1b3411ff321c2be0b85aef
greysoh <greysoh@pm.me>
2023-10-20 20:04:02 -0400

    fix: Fixes Firefox support when logged in.

--- src/index.mjs
+++ src/index.mjs
@@ -9,12 +9,16 @@
 // @run-at       document-start
 // ==/UserScript==
 
+const isLoggedInTextSelector = "span.yt-core-attributed-string.yt-core-attributed-string--white-space-no-wrap";
+
 const firstSectionExpandedButtonsSelector = "div#items.style-scope.ytd-guide-section-renderer";
 const sideButtonsSelector = "ytd-mini-guide-entry-renderer.style-scope.ytd-mini-guide-renderer";
 const shortieSelector = "ytd-rich-shelf-renderer.style-scope.ytd-rich-section-renderer";
 
 let oldHref = window.location.href;
 let isRunningAdObserver = window.location.href.includes("watch?v=");
+
+let isLoggedIn = false;
 
 const observer = new MutationObserver((mutations) => {
   if (oldHref != window.location.href) {
@@ -24,11 +28,13 @@ const observer = new MutationObserver((mutations) => {
 });
 
 async function doMainPageAdsChecker() {
+  let offset = !isLoggedIn ? -1 : 0;
+
   const sideButtons = document.querySelectorAll(sideButtonsSelector);
-  if (sideButtons && sideButtons.length == 5) sideButtons[1].remove();
+  if (sideButtons && sideButtons.length == 5+offset) sideButtons[1].remove();
 
   const sectionLists = document.querySelectorAll(firstSectionExpandedButtonsSelector);
-  if (sectionLists.length != 0 && sectionLists[0].childNodes.length == 3) sectionLists[0].childNodes[1].remove();
+  if (sectionLists.length != 0 && sectionLists[0].childNodes.length == 3-offset) sectionLists[0].childNodes[1].remove();
 
   document.querySelectorAll(shortieSelector).forEach((i) => i.remove());
 }
@@ -42,6 +48,8 @@ async function oneTrueMain() {
 }
 
 document.addEventListener("DOMContentLoaded", (e) => {
+  isLoggedIn = document.querySelectorAll(isLoggedInTextSelector).length == 2;
+
   oneTrueMain();
 
   observer.observe(document.querySelector("body"), {
